f1ed0a661117795edfc52c22731aec5ea4e56873
Sam Richard <snugug@google.com>
2022-07-18 07:48:27 -0500

    Add Fieldset

---
+++ src/lib/core/Fieldset.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Input from './Input.svelte';
+  export let field;
+  export let index = null;
+</script>
+
+<fieldset class="form--fieldset form--group">
+  <legend class="form--legend">{field.label}</legend>
+  {#each field.fields as f}
+    {#if f.fields}
+      <svelte:self bind:field={f} />
+    {:else}
+      <Input bind:field={f} name={index >= 0 ? field.name + `[${index}]` : field.name} />
+    {/if}
+  {/each}
+</fieldset>
